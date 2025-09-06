0972302b09cb4c1a994e07394653787e408366c0
ZuhdiJaara <118082936+ZuhdiJaara@users.noreply.github.com>
2025-09-07 01:22:55 +0300

    Initial Commit

---
+++ e shop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  e shop
+//
+//  Created by Zuhdi Jaara on 07/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ e shopTests/e_shopTests.swift
@@ -0,0 +1,17 @@
+//
+//  e_shopTests.swift
+//  e shopTests
+//
+//  Created by Zuhdi Jaara on 07/09/2025.
+//
+
+import Testing
+@testable import e_shop
+
+struct e_shopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
